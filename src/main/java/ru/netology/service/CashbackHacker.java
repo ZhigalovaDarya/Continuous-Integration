2343278ec81529a7a5ecc6b7c5e7caee37dabdbf
Toby Mory <darijigalova@yandex.ru>
2020-03-03 19:58:00 +0300

    CashbackHacker changed to CashBackHackService and Added Parametrized test

--- src/main/java/ru/netology/service/CashbackHacker.java
+++
@@ -1,9 +0,0 @@
-package ru.netology.service;
-
-public class CashbackHacker {
-    private final int boundary = 1000;
-
-    public int remain(int amount) {
-        return boundary - amount % boundary;
-    }
-}
---
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -0,0 +1,22 @@
+package ru.netology.service;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvFileSource;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CashbackHackServiceTest {
+
+    @ParameterizedTest
+    @CsvFileSource(resources = "/BonusData.csv", numLinesToSkip = 1)
+    void shouldCalculateAmountForBonus(int amount, int expected, String message) {
+
+        CashbackHackService cashbackHackService = new CashbackHackService();
+
+        int actual = cashbackHackService.remain(amount);
+
+        assertEquals(expected, actual, message);
+    }
+
+}
